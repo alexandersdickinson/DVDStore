b47e6d28c19360415077a297bcea843d5f8f45aa
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2016-10-02 13:58:37 -0700

    First commit.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DVDStore
+//
+//  Created by Alex Dickinson on 02.10.16.
+//  Copyright © 2016 Alex Dickinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
